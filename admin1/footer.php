005aac952aba236d927f944fddb7c5fb064e4a42
254francis <86851202+254francis@users.noreply.github.com>
2022-11-28 13:24:00 +0300

    Add files via upload

---
+++ admin1/footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+<hr>
+<p class="centered">Created by <a href="https://github.com/254francis">Francis Chege</a> & Albert Balimunsi</p>
+</div>
---
+++ admin1/user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                Created by <a href="https://github.com/254francis">Francis Chege</a> & Albert Balimunsi
+            </div>
+        </div>
+    </footer>
